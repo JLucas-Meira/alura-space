eec88aa1fc19180eb3d7094f4b38e13c3e9d8355
João Lucas <joaolcs842@gmail.com>
2025-01-20 23:09:53 -0300

    Imagens, filtros e novos caminhos

--- setup/settings.py
+++ setup/settings.py
@@ -127,6 +127,12 @@ STATICFILES_DIRS = [
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
+# Media
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'media')
+
+STATIC_URL = '/media/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
