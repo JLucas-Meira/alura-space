af959a1e73d74c00f322d46fb45e82db1fe99723
João Lucas <joaolcs842@gmail.com>
2025-01-25 17:56:44 -0300

    Formulário de Cadastro

--- usuarios/forms.py
+++ usuarios/forms.py
@@ -11,8 +11,7 @@ class Loginforms(forms.Form):
                 "placeholder": "Ex.: João Paulo Ferreira"
             }
         )
-    )
-    
+    ) 
     senha = forms.CharField(
         label = "Senha",
         required = True,
@@ -25,3 +24,48 @@ class Loginforms(forms.Form):
         )
     )
 
+class Cadastroforms(forms.Form):
+    nome_cadastro = forms.CharField(
+        label = "Nome de Cadastro",
+        required = True,
+        max_length = 100,
+        widget = forms.TextInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "Ex.: João Paulo Ferreira"
+            }
+        )
+    ) 
+    email_cadastro = forms.EmailField(
+        label = "Email",
+        required = True,
+        max_length = 100,
+        widget = forms.EmailInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "Ex.: jpferreira@xpto.com"
+            }
+        )
+    )
+    senha_1 = forms.CharField(
+        label = "Senha",
+        required = True,
+        max_length = 70,
+        widget = forms.PasswordInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "Digite sua senha"
+            }
+        )
+    )
+    senha_2 = forms.CharField(
+        label = "Confirme sua senha",
+        required = True,
+        max_length = 70,
+        widget = forms.PasswordInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "Digite sua senha novamente"
+            }
+        )
+    )
--- usuarios/views.py
+++ usuarios/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
-from usuarios.forms import Loginforms
+from usuarios.forms import Loginforms, Cadastroforms
 
 def login(request):
     form = Loginforms
     return render(request, 'usuarios/login.html', {'form': form})
 
 def cadastro(request):
-    return render(request, 'usuarios/cadastro.html')
+    form = Cadastroforms
+    return render(request, 'usuarios/cadastro.html', {'form': form})
