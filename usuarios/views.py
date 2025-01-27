c8571bbf7f963702a9a2af7e8c78b111b0217719
João Lucas <joaolcs842@gmail.com>
2025-01-27 20:05:57 -0300

    Cadastro de usuários

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,10 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from usuarios.forms import Loginforms, Cadastroforms
+from django.contrib.auth.models import User
 
 def login(request):
-    form = Loginforms
+    form = Loginforms()
     return render(request, 'usuarios/login.html', {'form': form})
 
 def cadastro(request):
-    form = Cadastroforms
+    form = Cadastroforms()
+    
+    if request.method == 'POST':
+        form = Cadastroforms(request.POST)
+
+        if form.is_valid():
+            if form['senha_1'].value() != form['senha_2'].value():
+                return redirect('cadastro')
+            
+            nome = form['nome_cadastro'].value()
+            email = form['email_cadastro'].value()
+            senha = form['senha_1'].value()
+
+            if User.objects.filter(username=nome).exists():
+                return redirect('cadastro')
+            
+            usuario = User.objects.create_user(
+                username = nome,
+                email = email,
+                password = senha
+            )
+            usuario.save()
+            return redirect('login')
+    
+
     return render(request, 'usuarios/cadastro.html', {'form': form})
